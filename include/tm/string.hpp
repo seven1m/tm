ac56dfdef4c18c9bd02293cfb40f3bc1ed868bab
Tim Morgan <tim@timmorgan.org>
2022-04-16 21:28:43 -0500

    Add '+' operator for String

--- include/tm/string.hpp
+++ include/tm/string.hpp
@@ -229,6 +229,23 @@ public:
     }
 
     /**
+     * Appends two Strings together and returns the result.
+     *
+     * ```
+     * auto str1 = String { "foo" };
+     * auto str2 = String { "bar" };
+     * assert_str_eq("foobar", str1 + str2);
+     *
+     * assert_str_eq("12", String("1") + "2");
+     * ```
+     */
+    String operator+(const String &other) const {
+        auto new_string = String(*this);
+        new_string.append(other);
+        return new_string;
+    }
+
+    /**
      * Replaces the String data by copying from an a C string.
      *
      * ```
