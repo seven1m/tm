4cefd4f46c674ba493f5fd9b8803645b978ebfa0
Herwin <herwinw@users.noreply.github.com>
2025-01-24 17:56:05 +0100

    Group String::operator= functions in header file

--- include/tm/string.hpp
+++ include/tm/string.hpp
@@ -377,23 +377,6 @@ public:
     }
 
     /**
-     * Appends two Strings together and returns the result.
-     *
-     * ```
-     * auto str1 = String { "foo" };
-     * auto str2 = String { "bar" };
-     * assert_str_eq("foobar", str1 + str2);
-     *
-     * assert_str_eq("12", String("1") + "2");
-     * ```
-     */
-    String operator+(const String &other) const {
-        auto new_string = String(*this);
-        new_string.append(other);
-        return new_string;
-    }
-
-    /**
      * Replaces the String data by copying from an a C string.
      *
      * ```
@@ -409,6 +392,23 @@ public:
         }
         set_str(other);
         return *this;
+    }
+
+    /**
+     * Appends two Strings together and returns the result.
+     *
+     * ```
+     * auto str1 = String { "foo" };
+     * auto str2 = String { "bar" };
+     * assert_str_eq("foobar", str1 + str2);
+     *
+     * assert_str_eq("12", String("1") + "2");
+     * ```
+     */
+    String operator+(const String &other) const {
+        auto new_string = String(*this);
+        new_string.append(other);
+        return new_string;
     }
 
     /**
@@ -1898,4 +1898,5 @@ protected:
     size_t m_length { 0 };
     size_t m_capacity { 0 };
 };
+
 }
